f66dbc7b2fd7149246e3759375099bd29013a455
Shashwat-DS <61488456+shashwat-dash@users.noreply.github.com>
2021-01-21 23:21:41 +0530

    Add files via upload

---
+++ Rock Paper Scissor.py
@@ -0,0 +1,50 @@
+
+#Rock Paper Scissors
+
+import random
+
+def gameWin(comp, you):
+
+    if comp == you:          # Tie!
+        return None
+
+       elif comp == 's':     # Possibilities when computer chose s
+        if you == 'w':
+            return False
+        elif you == 'g':
+            return True
+
+    elif comp == 'w':        # Possibilities when computer chose w
+        if you == 'g':
+            return False
+        elif you == 's':
+            return True
+
+    elif comp == 'g':        # Possibilities when computer chose g
+        if you == 's':
+            return False
+        elif you == 'w':
+            return True
+
+
+print("Comp Turn: Snake(s) Water(w) or Gun(g)?")
+randNo = random.randint(1, 3)
+if randNo == 1:
+    comp = 's'
+elif randNo == 2:
+    comp = 'w'
+elif randNo == 3:
+    comp = 'g'
+
+you = input("Your Turn: Snake(s) Water(w) or Gun(g)?")
+a = gameWin(comp, you)
+
+print(f"Computer chose {comp}")
+print(f"You chose {you}")
+
+if a == None:
+    print("The game is a tie!")
+elif a:
+    print("You Win!")
+else:
+    print("You Lose!")
